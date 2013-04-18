d2f2b82d9b770676a53b1a5c95b6397ffad516d6
Jef Claes <jef.claes@gmail.com>
2013-04-18 19:15:25 +0200

    Added test for guards.

---
+++ src/ModelingFourEyes/ModelingFourEyes.Test/WhenGuarding.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModelingFourEyes.Test
+{
+    [TestClass]
+    public class WhenGuarding
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ForEmptyString()
+        {
+            Guard.ForEmpty(string.Empty, "string");      
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ForEmptyObject()
+        {
+            Guard.ForEmpty((object)null, "object");
+        }
+    }
+}
